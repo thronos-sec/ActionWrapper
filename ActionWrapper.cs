1a98aabf6669ff129bf014f735c53258b6a1c3e6
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2025-07-31 23:09:06 -0300

    1.0

---
+++ ActionWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace ActionWrapper;
+
+public class ActionWrapper
+{
+    private readonly Action _action;
+
+    public ActionWrapper(Action action)
+    {
+        _action = action ?? throw new ArgumentNullException(nameof(action));
+    }
+
+    public void Execute()
+    {
+        _action();
+    }
+}
