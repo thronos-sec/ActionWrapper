00d17cda744a09becc6640ec116551a0cb599a84
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2025-08-08 10:51:49 -0300

    Adding real RecurringJobExtension

---
+++ RecurringJobExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Hangfire;
+
+namespace Hangfire.RecurringJobs.Extensions;
+
+public static class DynamicRecurringJob
+{
+
+    public delegate void DynamicAction(string? stateSerialized);
+    private static DynamicAction? Action { get; set; }
+
+    public static void SetAction(DynamicAction action)
+    {
+        if (Action == null)
+        {
+            Action = action;
+        }
+    }
+
+    public static void AddOrUpdate(string recurringJobId, string cronExpression, object? state = null, JsonSerializerOptions? serializerOptions = null)
+    {
+        string objectJson = JsonSerializer.Serialize(state, serializerOptions);
+        RecurringJob.AddOrUpdate(recurringJobId, () => DynamicExecution(objectJson), cronExpression);
+    }
+
+    private static void DynamicExecution(string? stateSerialized = null)
+    {
+        
+        Action?.Invoke(stateSerialized);
+    }
+}
+
