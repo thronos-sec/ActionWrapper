19e3b01477a2a3339cc41f94841295297f2d1ce1
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2025-08-15 23:04:27 -0300

    Adding destination

--- RecurringJobExtension.cs
+++ RecurringJobExtension.cs
@@ -5,7 +5,7 @@ namespace Hangfire.RecurringJobs.Extensions;
 public static class DynamicRecurringJob
 {
 
-    public delegate void DynamicAction(string? stateSerialized);
+    public delegate void DynamicAction(string destination, string? stateSerialized);
     private static DynamicAction? Action { get; set; }
 
     public static void SetAction(DynamicAction action)
@@ -16,16 +16,15 @@ public static class DynamicRecurringJob
         }
     }
 
-    public static void AddOrUpdate(string recurringJobId, string cronExpression, object? state = null, JsonSerializerOptions? serializerOptions = null)
+    public static void AddOrUpdate(string recurringJobId, string cronExpression, string destination, object? state = null, JsonSerializerOptions? serializerOptions = null)
     {
         string objectJson = JsonSerializer.Serialize(state, serializerOptions);
-        RecurringJob.AddOrUpdate(recurringJobId, () => DynamicExecution(objectJson), cronExpression);
+        RecurringJob.AddOrUpdate(recurringJobId, () => DynamicExecution(destination, objectJson), cronExpression);
     }
 
-    private static void DynamicExecution(string? stateSerialized = null)
+    private static void DynamicExecution(string destination, string stateSerialized)
     {
-        
-        Action?.Invoke(stateSerialized);
+        Action?.Invoke(destination, stateSerialized);
     }
 }
 
